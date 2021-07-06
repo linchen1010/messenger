aba9ded1b439b1df85619f2a5cfb80d973c4b9be
SHI-LIN CHEN <shilinch@usc.edu>
2021-07-06 11:04:31 -0700

    add a helper function to fetch contacts of users

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,6 +1,7 @@
 const { Op } = require("sequelize");
 const db = require("../db");
 const Message = require("./message");
+const User = require("./user");
 
 const Conversation = db.define("conversation", {});
 
@@ -38,6 +39,28 @@ Conversation.checkConversationOwner = async function (conversationId, userId) {
   });
   if (conversation) return true;
   return false;
+};
+
+// get the contacts (id) of user given userId
+
+Conversation.getUserContacts = async (userId) => {
+  const conversations = await Conversation.findAll({
+    where: {
+      [Op.or]: {
+        user1Id: userId,
+        user2Id: userId,
+      },
+    },
+    attribute: ["id"],
+  });
+  const userIdSet = new Set();
+  for (let i = 0; i < conversations.length; i++) {
+    userIdSet.add(conversations[i].dataValues.user1Id);
+    userIdSet.add(conversations[i].dataValues.user2Id);
+  }
+  userIdSet.delete(userId);
+  const userContacts = Array.from(userIdSet);
+  return userContacts;
 };
 
 module.exports = Conversation;
